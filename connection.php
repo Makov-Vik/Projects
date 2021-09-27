44ba3b12cd1f95cdcb0ad9d869d80f5839dd399c
Makov-Vik <72148650+Makov-Vik@users.noreply.github.com>
2021-09-27 12:41:17 +0300

    Delete connection.php

--- connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-  $serverMySQL="cipher.com"; # имя сервера
-  $db="cipher_db"; # база данных
-  $dblog="mysql"; # логин
-  $dbpass=""; # пароль
-  $table="history"; # таблица
-  
-  $connection = mysqli_connect($serverMySQL, $dblog, $dbpass, $db);
-?>
